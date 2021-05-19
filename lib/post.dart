7b3bc9b8d36be2c13201330e910ea7c2a5b9dca9
spencejax21 <spencejax21@gmail.com>
2021-05-19 13:00:18 -0400

    Add header to post cards

--- lib/post.dart
+++ lib/post.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:music_app/styles.dart';
 import 'package:flutter/cupertino.dart';
 
+// ignore: must_be_immutable
 class Post extends StatefulWidget {
   String _song;
 
@@ -19,6 +20,7 @@ class _PostState extends State<Post> {
       //elevation: 10,
       child: Container(
         child: Container(
+          height: 250,
           decoration: BoxDecoration(
             border: Border.all(
               color: Colors.transparent
@@ -26,8 +28,32 @@ class _PostState extends State<Post> {
             color: Styles.postColor,
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Text(this.widget._song),
-          height: 250
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      child: Image.asset('assets/images/ryancircle.png', width: 20, height: 20,),
+                      padding: EdgeInsets.only(right: 5)
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: Colors.white),
+                        children: <TextSpan>[
+                          TextSpan(text: 'Ryan Gosling', style: TextStyle(fontWeight: FontWeight.bold)),
+                          TextSpan(text: ' posted a '),
+                          TextSpan(text: 'song', style: TextStyle(color: Styles.secondaryColor,))
+                        ]
+                      )
+                    )
+                  ],
+                )
+              ],
+              crossAxisAlignment: CrossAxisAlignment.start,
+            )
+          )  
         ),
         color: Styles.backgroundColor
       )
